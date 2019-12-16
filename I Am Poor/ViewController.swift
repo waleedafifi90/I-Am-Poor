e748c684b85d1e0f8cae1272363732fa02ba9284
Waleed A. Afifi <waleedafifich@gmail.com>
2019-12-16 23:57:41 +0200

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Waleed Afifi on 16/12/2019.
+//  Copyright © 2019 Waleed Afifi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
